72fddc9e7824f16a22971ac7f29077654946d856
KatMoura <kmouramonteiro@gmail.com>
2026-03-10 13:46:17 -0300

    Atualizando

--- static/config.js
+++ static/config.js
@@ -3,6 +3,6 @@ window.__APP_CONFIG__ = window.__APP_CONFIG__ || {
     // Example:
     // API_ORDERS: 'https://api.seudominio.com',
     // API_USERS: 'https://api.seudominio.com',
-    API_ORDERS: '',
-    API_USERS: ''
+    API_ORDERS: 'https://sistema-pedidos-2.onrender.com',
+    API_USERS: 'https://sistema-pedidos-2.onrender.com'
 };
